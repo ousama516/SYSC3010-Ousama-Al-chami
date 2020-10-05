dbb9d3c818c7472bb8152c1339db0bf91535da96
pc <OUSAMAALCHAMI@CMAIL.CARLETON.CA>
2020-10-05 16:51:30 -0400

    this will be the lab4 task

---
+++ lab4/lab4.py
@@ -0,0 +1,22 @@
+api_key="54CDA6FJF4RCJ92C"
+
+from urllib.parse import urlencode
+import http.client as httplib
+
+
+#this sets the up teh url for thing speak
+params = urlencode({'field1':"ousamaalchami@cmail.carleton.ca",'field2':"L2-M-4",'field3':"b", 'key': api_key})
+headers = {"Content-typZZe": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+conn = httplib.HTTPConnection("api.thingspeak.com:80")
+#this will try to send the roomtemp to thing speak
+try:
+
+    conn.request("POST", "/update", params, headers)
+    response = conn.getresponse()
+    print(response.status, response.reason)
+    conn.close()
+    #IF CONNECTION FAILS SEND AN ERROR
+except:
+    print("connection failed")
+
